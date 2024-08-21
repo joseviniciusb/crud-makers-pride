18a651b812cfe87cf2df434a2e59e653c5e79fda
José Vinicius <93006559+joseviniciusb@users.noreply.github.com>
2024-08-21 02:30:51 +0000

    feat: create routes in app.ts

--- src/App.ts
+++ src/App.ts
@@ -4,4 +4,20 @@ const app = express();
 
 app.get("/", function (req, res) {
   res.send("Hello World!");
+  console.log(req);
+});
+
+app.post("/", function (req, res) {
+  res.send("Got a POST request");
+  console.log(req);
+});
+
+app.put("/user", function (req, res) {
+  res.send("Got a PUT request at /user");
+  console.log(req);
+});
+
+app.delete("/user", function (req, res) {
+  res.send("Got a DELETE request at /user");
+  console.log(req);
 });
